e632308da3b3cc6477526be3895eca56acc1a05d
Ashley <ashleych@AshleydeMacBook-Air.local>
2026-05-21 22:55:23 +0800

    Fix PWA start_url for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'fittrack-v1'
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']
+const ASSETS = ['/fittrack/', '/fittrack/index.html', '/fittrack/manifest.json', '/fittrack/icon-192.png', '/fittrack/icon-512.png']
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => !a.endsWith('.png') || true))))
